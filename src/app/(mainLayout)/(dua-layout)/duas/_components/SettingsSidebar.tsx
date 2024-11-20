80acb1846fbe9ba5635ae621cb64eefbc9ba9ab4
Ali Ashraf <89378903+webashraf@users.noreply.github.com>
2024-11-20 22:48:00 +0600

    add settigns sidebar

---
+++ src/app/(mainLayout)/(dua-layout)/duas/_components/SettingsSidebar.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+import { Switch } from "@nextui-org/switch";
+import { cn } from "@nextui-org/theme";
+import { useState } from "react";
+import { HiOutlineLanguage } from "react-icons/hi2";
+import { RxDashboard } from "react-icons/rx";
+
+const SettingsSidebar = () => {
+  const [activeTab, setActiveTab] = useState("appearance");
+  const [nightMode, setNightMode] = useState(false);
+
+  const tabs = [
+    { id: "language", name: "Language Settings", icon: <HiOutlineLanguage /> },
+    { id: "general", name: "General Settings", icon: <RxDashboard /> },
+    { id: "font", name: "Font Settings", icon: <RxDashboard /> },
+    { id: "appearance", name: "Appearance Settings", icon: <RxDashboard /> },
+  ];
+
+  return (
+    <div className="w-full max-w-sm mx-auto p-4 h-full rounded-lg shadow">
+      <h2 className="text-lg font-semibold text-gray-800 mb-4 text-center">
+        Settings
+      </h2>
+      <div className="flex flex-col space-y-2">
+        {tabs.map((tab) => (
+          <button
+            key={tab.id}
+            className={`flex items-center px-4 py-2 rounded-sm text-sm font-medium border-l-4 border-l-transparent ${
+              activeTab === tab.id
+                ? "bg-white  border-l-green-600 text-green-700"
+                : "text-gray-700 hover:bg-gray-100"
+            }`}
+            onClick={() => setActiveTab(tab.id)}
+          >
+            <p className="flex items-center gap-2">
+              {" "}
+              {tab.icon} {tab.name}
+            </p>
+          </button>
+        ))}
+      </div>
+      <div className="mt-6">
+        {activeTab === "appearance" && (
+          <div className="flex items-center justify-between">
+            <h3 className="text-sm font-medium text-gray-800">Night Mode</h3>
+
+            <Switch
+              classNames={{
+                base: cn(
+                  "inline-flex flex-row-reverse w-full  items-center",
+                  "justify-between cursor-pointer rounded-lg gap-2 px-4 border-2 border-transparent",
+                  ""
+                ),
+                wrapper: "p-0 h-4 overflow-visible",
+                thumb: cn(
+                  "w-6 h-6 border-2 shadow-lg",
+                  "group-data-[hover=true]:border-primary",
+                  //selected
+                  "group-data-[selected=true]:ml-6",
+                  // pressed
+                  "group-data-[pressed=true]:w-7",
+                  "group-data-[selected]:group-data-[pressed]:ml-4"
+                ),
+              }}
+            />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default SettingsSidebar;
--- src/app/(mainLayout)/(dua-layout)/layout.tsx
+++ src/app/(mainLayout)/(dua-layout)/layout.tsx
@@ -1,5 +1,7 @@
 import { ReactNode } from "react";
 
+import SettingsSidebar from "./duas/_components/SettingsSidebar";
+
 import CategoryMenu from "@/src/components/categoryMenu";
 import { Navbar } from "@/src/components/navbar";
 import SideIconNav from "@/src/components/sideIconNav";
@@ -19,7 +21,9 @@ const layout = ({ children }: { children: ReactNode }) => {
         <div className="min-w-[50%] max-w-[60%]  h-[90vh] overflow-y-scroll custom-scrollbar">
           {children}
         </div>
-        <div className="w-[350px] bg-white h-[90vh] rounded-xl">Right</div>
+        <div className="w-[400px] bg-white h-[90vh] rounded-3xl overflow-hidden">
+          <SettingsSidebar />
+        </div>
       </div>
     </div>
   );
