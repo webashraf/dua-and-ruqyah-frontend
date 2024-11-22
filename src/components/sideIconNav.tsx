e86ee2e8b4637e0a6b2ce19a9fa523bc911e50aa
Ali Ashraf <89378903+webashraf@users.noreply.github.com>
2024-11-22 22:05:58 +0600

    improve code for deploy issue

--- src/components/sideIconNav.tsx
+++ src/components/sideIconNav.tsx
@@ -1,90 +1,60 @@
 import Image from "next/image";
 
+const icons = [
+  { src: "https://i.ibb.co/278fYn2/home-1.png", alt: "Home" },
+  { src: "https://i.ibb.co/GCbZHFX/54-menu-2.png", alt: "Menu" },
+  { src: "https://i.ibb.co/Tv6Gx0G/memorize-1.png", alt: "Memorize" },
+  { src: "https://i.ibb.co/Wz5CnVM/3-keep-minus.png", alt: "Keep Minus" },
+  { src: "https://i.ibb.co/Wz5CnVM/3-keep-minus.png", alt: "Another Icon" },
+  {
+    src: "https://i.ibb.co/6FVvpgn/ruqyah-1.png",
+    alt: "Ruqyah",
+    hiddenOnSmall: true,
+  },
+  {
+    src: "https://i.ibb.co/NLxpC8X/dua-info-1.png",
+    alt: "Dua Info",
+    hiddenOnSmall: true,
+  },
+  {
+    src: "https://i.ibb.co/9gCDMjY/books-1.png",
+    alt: "Books",
+    hiddenOnSmall: true,
+  },
+];
+
 const SideIconNav = () => {
   return (
-    <div className="lg:h-[92vh] mt-10 rounded-3xl bg-white flex flex-col justify-between items-center py-5 lg:min-w-[100px]  min-w-[100vw] lg:static fixed bottom-0 z-20">
+    <div className="lg:h-[92vh] mt-10 rounded-3xl bg-white flex flex-col justify-between items-center py-5 lg:min-w-[100px] min-w-[100vw] lg:static fixed bottom-0 z-20">
+      {/* Logo */}
       <div className="hidden lg:block">
         <Image
-          alt="dua-logo"
-          className=""
+          alt="Dua Logo"
           height={90}
           src="https://i.ibb.co/ZTCrp4L/unnamed-1.png"
           width={90}
         />
       </div>
 
-      <div className="flex lg:flex-col gap-[27]">
-        <div className="size-[38] bg-[#e8f0f5] flex justify-center items-center rounded-full">
-          <Image
-            alt="dua-logo"
-            height={20}
-            src="https://i.ibb.co/278fYn2/home-1.png"
-            width={20}
-          />
-        </div>
-        <div className="size-[38] bg-[#e8f0f5] flex justify-center items-center rounded-full">
-          <Image
-            alt="dua-logo"
-            height={20}
-            src="https://i.ibb.co/GCbZHFX/54-menu-2.png"
-            width={20}
-          />
-        </div>
-        <div className="size-[38] bg-[#e8f0f5] flex justify-center items-center rounded-full">
-          <Image
-            alt="dua-logo"
-            height={20}
-            src="https://i.ibb.co/Tv6Gx0G/memorize-1.png"
-            width={20}
-          />
-        </div>
-        <div className="size-[38] bg-[#e8f0f5] flex justify-center items-center rounded-full">
-          <Image
-            alt="dua-logo"
-            height={20}
-            src="https://i.ibb.co/Wz5CnVM/3-keep-minus.png"
-            width={20}
-          />
-        </div>
-        <div className="size-[38] bg-[#e8f0f5] flex justify-center items-center rounded-full">
-          <Image
-            alt="dua-logo"
-            height={20}
-            src="https://i.ibb.co/Wz5CnVM/3-keep-minus.png"
-            width={20}
-          />
-        </div>
-
-        <div className="size-[38] bg-[#e8f0f5] lg:flex hidden justify-center items-center rounded-full">
-          <Image
-            alt="dua-logo"
-            height={20}
-            src="https://i.ibb.co/6FVvpgn/ruqyah-1.png"
-            width={20}
-          />
-        </div>
-        <div className="size-[38] bg-[#e8f0f5] lg:flex hidden justify-center items-center rounded-full">
-          <Image
-            alt="dua-logo"
-            height={20}
-            src="https://i.ibb.co/NLxpC8X/dua-info-1.png"
-            width={20}
-          />
-        </div>
-        <div className="size-[38] bg-[#e8f0f5] lg:flex justify-center items-center rounded-full hidden">
-          <Image
-            alt="dua-logo"
-            height={20}
-            src="https://i.ibb.co/9gCDMjY/books-1.png"
-            width={20}
-          />
-        </div>
+      {/* Icons */}
+      <div className="flex lg:flex-col gap-7">
+        {icons.map((icon, index) => (
+          <div
+            key={index}
+            className={`h-[38px] w-[38px] bg-[#e8f0f5] flex justify-center items-center rounded-full ${
+              icon.hiddenOnSmall ? "hidden lg:flex" : ""
+            }`}
+          >
+            <Image alt={icon.alt} height={20} src={icon.src} width={20} />
+          </div>
+        ))}
       </div>
 
-      <div className="lg:block hidden">
-        <div className="bg-primary size-[57px] flex justify-center items-center rounded-lg">
+      {/* Bottom Icon */}
+      <div className="hidden lg:block">
+        <div className="bg-primary h-[57px] w-[57px] flex justify-center items-center rounded-lg">
           <Image
-            alt="dua-logo"
+            alt="Bottom Icon"
             height={24}
             src="https://i.ibb.co/mDqPmnS/icon.png"
             width={24}
